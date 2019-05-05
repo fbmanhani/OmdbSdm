abb21328e576a6f415df37717a1a54a95cd50514
Felipe Barbosa Manhani <fbmanhani@gmail.com>
2019-05-05 12:03:00 -0300

    Initia commit

---
+++ app/src/main/java/br/edu/ifsp/sdm/manhani/omdbsdm/Constantes.kt
@@ -0,0 +1,8 @@
+package br.edu.ifsp.sdm.manhani.omdbsdm
+
+object Constantes {
+    // Constantes usadas para acesso ao WS de tradução
+    val URL_BASE = "http://www.omdbapi.com/"
+    val KEY_FIELD = "app_key"
+    val KEY_VALUE = "830b2d70"
+}
